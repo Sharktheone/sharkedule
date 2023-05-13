cfcb1abc3436a4f32bf54e88786610c7dfb2befc
Sharktheone <sharktheone@proton.me>
2023-05-13 23:56:02 +0200

    make task name editable

--- src/pages/task/kanban/column/task/task.tsx
+++ src/pages/task/kanban/column/task/task.tsx
@@ -1,6 +1,7 @@
-import {Flex} from "@mantine/core"
+import {Flex, Text} from "@mantine/core"
 import {useStyles} from "./styles"
 import {kanbanTaskType} from "../../types"
+import {useRef, useState} from "react"
 
 type TaskProps = {
     task: kanbanTaskType
@@ -9,10 +10,31 @@ type TaskProps = {
 
 export default function Task({task}: TaskProps) {
     const {classes, cx} = useStyles();
+    const [name, setName] = useState(task.name)
+    const ref = useRef<HTMLDivElement | null>(null)
+
+    ref?.current?.addEventListener('change', function () {
+        console.log(ref?.current?.innerText)
+    })
+
+    function editText() {
+        ref!.current!.contentEditable = "true"
+    }
+
+    function handleChange(e: any) {
+        setName(e.target.innerText)
+    }
+
+    function handleBlur(e: any) {
+        ref!.current!.contentEditable = "false"
+        console.log(e.target.innerText)
+    }
 
     return (
         <Flex className={cx(classes.task)}>
-            {task.name}
+            <Text onClick={editText} onInput={handleChange} onBlur={handleBlur} ref={ref} w="100%" h="100%">
+                {task.name}
+            </Text>
         </Flex>
     )
 }
