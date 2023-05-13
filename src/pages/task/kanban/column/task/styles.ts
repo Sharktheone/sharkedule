1667bf843756832434d41bcae31bab3516b18f52
Sharktheone <sharktheone@proton.me>
2023-05-13 20:53:29 +0200

    update task styles

--- src/pages/task/kanban/column/task/styles.ts
+++ src/pages/task/kanban/column/task/styles.ts
@@ -1,8 +1,9 @@
-import {createStyles} from "@mantine/core";
+import {createStyles} from "@mantine/core"
 
 
 export const useStyles = createStyles((theme) => ({
     task: {
+        minHeight: "3rem",
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
         border: `1px solid ${
             theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
@@ -11,7 +12,5 @@ export const useStyles = createStyles((theme) => ({
         padding: theme.spacing.xs,
         marginBottom: theme.spacing.xs,
         boxShadow: theme.shadows.sm,
-
-
     }
 }))
