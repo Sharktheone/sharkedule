5445663aaf1074cd43770e7d06dd3bad43c868b6
Sharktheone <sharktheone@proton.me>
2023-05-16 22:38:38 +0200

    create board button

--- src/pages/task/kanbanboards.tsx
+++ src/pages/task/kanbanboards.tsx
@@ -6,6 +6,9 @@ import {useColors} from "./styles"
 import styles from "./styles.module.scss"
 import CreateNewModal from "./createNewModal"
 import {useDisclosure} from "@mantine/hooks"
+import {api} from "../../api/api"
+import {notifications} from "@mantine/notifications"
+import {IconX} from "@tabler/icons-react"
 
 
 export default function KanbanBoards() {
@@ -16,8 +19,24 @@ export default function KanbanBoards() {
     const [boardNames, setBoardNames] = useState(loaderData as kanbanBoardType[])
     const [newOpened, {open, close}, ] = useDisclosure(false)
 
-    function newBoard() {
+    function openNewBoard() {
         open()
+    }
+
+    function createBoard(name: string) {
+        console.log(name)
+        api.put("/task/new", {name: name}).then(
+            (res) => {
+                if (res.status > 300) {
+                    console.log(res)
+                    console.log("hello1")
+                    notifications.show({title: "Error", message: "res.data", color: "red", icon: <IconX/>})
+                    console.log("hello")
+                }
+
+            }).catch(e => {
+            console.log(e)
+        })
     }
 
     return (
