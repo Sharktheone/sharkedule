107f03fedf2c458df1411ebd547801b19e0fb8c0
Sharktheone <sharktheone@proton.me>
2023-05-16 22:48:25 +0200

    show notification and redirect to new Board

--- src/pages/task/kanbanboards.tsx
+++ src/pages/task/kanbanboards.tsx
@@ -1,11 +1,11 @@
 import {useState} from "react"
 import {kanbanBoardType} from "./kanban/types"
-import {Link, useLoaderData} from "react-router-dom"
+import {Link, useLoaderData, useNavigate, useRevalidator} from "react-router-dom"
 import {Button, Container, Title} from "@mantine/core"
 import {useColors} from "./styles"
 import styles from "./styles.module.scss"
 import CreateNewModal from "./createNewModal"
-import {useDisclosure} from "@mantine/hooks"
+import {useDisclosure, useTimeout} from "@mantine/hooks"
 import {api} from "../../api/api"
 import {notifications} from "@mantine/notifications"
 import {IconX} from "@tabler/icons-react"
@@ -13,11 +13,12 @@ import {IconX} from "@tabler/icons-react"
 
 export default function KanbanBoards() {
     let loaderData = useLoaderData()
+    let navigate = useNavigate()
 
     let {classes, cx} = useColors()
 
     const [boardNames, setBoardNames] = useState(loaderData as kanbanBoardType[])
-    const [newOpened, {open, close}, ] = useDisclosure(false)
+    const [newOpened, {open, close},] = useDisclosure(false)
 
     function openNewBoard() {
         open()
@@ -32,6 +33,10 @@ export default function KanbanBoards() {
                     console.log("hello1")
                     notifications.show({title: "Error", message: "res.data", color: "red", icon: <IconX/>})
                     console.log("hello")
+                } else {
+                    notifications.show({title: "Success", message: "Board created", color: "green"})
+
+                    navigate(`${res.data.uuid}`)
                 }
 
             }).catch(e => {
@@ -43,7 +48,8 @@ export default function KanbanBoards() {
         <Container className={`${styles.boards} ${cx(classes.colors)}`}>
             <div>
                 <Title>Your Boards</Title>
-                <Button variant="gradient" gradient={{from: "yellow", to: "red"}} onClick={openNewBoard}>New Board</Button>
+                <Button variant="gradient" gradient={{from: "yellow", to: "red"}} onClick={openNewBoard}>New
+                    Board</Button>
             </div>
             <CreateNewModal close={close} opened={newOpened} handleCreate={createBoard}/>
 
