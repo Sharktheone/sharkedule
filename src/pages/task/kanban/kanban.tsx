59fd8972fb00f44b4f79771491321a14942e9268
Sharktheone <sharktheone@proton.me>
2023-05-23 23:46:51 +0200

    clean code (use handlers)

--- src/pages/task/kanban/kanban.tsx
+++ src/pages/task/kanban/kanban.tsx
@@ -10,86 +10,23 @@ import {useStyles} from "./styles"
 import {api} from "../../../api/api"
 import {notifications} from "@mantine/notifications"
 import {dragHandlers} from "./dragHandlers"
+import { handlers } from './handlers'
 
 export default function Kanban() {
     const loaderData = useLoaderData()
     const [board, setBoard] = useState<kanbanBoardType>(loaderData as kanbanBoardType)
+
     const navigate = useNavigate()
     const [isAdding, setIsAdding] = useState(false)
     const newColRef = useRef<HTMLInputElement>(null)
-    const [removeTimeout, setRemoveTimeout] = useState<number | undefined>(undefined)
-
     const {classes, cx} = useStyles()
 
     const drag = new dragHandlers(board, setBoard)
+    const h = new handlers(board, setBoard, setIsAdding, newColRef)
 
     useEffect(() => {
         setBoard(loaderData as kanbanBoardType)
     }, [loaderData])
-
-    function renameTask(uuid: string, name: string) {
-        let newBoard = {...board}
-        newBoard.columns?.forEach((column) => {
-            column.tasks?.forEach((task) => {
-                if (task.uuid === uuid) {
-                    task.name = name
-                    return
-                }
-            })
-        })
-        setBoard(newBoard)
-    }
-
-    function renameColumn(uuid: string, name: string) {
-        let newBoard = {...board}
-        newBoard.columns?.forEach((column) => {
-            if (column.uuid === uuid) {
-                column.name = name
-                return
-            }
-        })
-        setBoard(newBoard)
-    }
-
-    function handleNewColumn() {
-        setIsAdding(true)
-    }
-
-    function cancelAddColumn() {
-        setRemoveTimeout(
-            setTimeout(() => {
-                setIsAdding(false)
-            }, 100)
-        )
-    }
-
-    function addColumn() {
-        if (removeTimeout) clearTimeout(removeTimeout)
-
-        const name = newColRef.current?.value
-        if (!name) {
-            notifications.show({title: "Error", message: "Column name cannot be empty", color: "red"})
-            return
-        }
-
-        api.put(`/kanbanboard/${board.uuid}/column/new`, {name: name}).then(
-            (res) => {
-                if (res.status > 300) {
-                    notifications.show({title: "Error", message: res.data, color: "red"})
-                    console.log(res)
-                } else {
-                    notifications.show({title: "Success", message: "Column created", color: "green"})
-                    if (newColRef.current) newColRef.current.value = ""
-
-                    navigate("")
-                }
-            }).catch(
-            (err) => {
-                notifications.show({title: "Error", message: err.message, color: "red"})
-                console.log(err)
-            }
-        )
-    }
 
     useEffect(() => {
         newColRef?.current?.focus()
@@ -109,9 +46,9 @@ export default function Kanban() {
                             <div className={styles.cols}>
                                 {board.columns?.map((column) => (
                                     <div key={column.uuid}>
-                                        <Column column={column} renameColumn={renameColumn}
+                                        <Column column={column} renameColumn={h.renameColumn}
                                                 index={board.columns?.indexOf(column) ?? 0}
-                                                renameTask={renameTask}
+                                                renameTask={h.renameTask}
                                                 boardUUID={board.uuid} ghost={drag.ghost}/>
                                     </div>
                                 ))}
@@ -121,17 +58,17 @@ export default function Kanban() {
 
                                 {!isAdding ?
                                     <>
-                                        <button onClick={handleNewColumn}
+                                        <button onClick={h.handleNewColumn}
                                                 className={`${cx(classes.addColumn)} ${styles.footer}`}>
                                             <IconPlus size={24}/>
                                             <Text align="center">Add a Column</Text>
                                         </button>
                                     </> :
                                     <Stack className={styles.add}>
-                                        <Input ref={newColRef} onBlur={cancelAddColumn}
+                                        <Input ref={newColRef} onBlur={h.cancelAddColumn}
                                                placeholder="Column name"></Input>
                                         <div className={styles.menu}>
-                                            <Button onClick={addColumn} gradient={{from: "#6dd6ed", to: "#586bed"}}
+                                            <Button onClick={h.addColumn} gradient={{from: "#6dd6ed", to: "#586bed"}}
                                                     variant="gradient">Create</Button>
                                             <CloseButton onClick={() => setIsAdding(false)}/>
                                         </div>
