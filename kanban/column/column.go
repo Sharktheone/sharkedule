936564977112ce202089ce73ba487fdf0e0d4f05
Sharktheone <sharktheone@proton.me>
2023-05-26 21:35:28 +0200

    col utils

---
+++ kanban/column/column.go
@@ -0,0 +1,43 @@
+package column
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"sharkedule/kanban"
+	"sharkedule/kanban/KTypes"
+)
+
+func getColumn(board interface{}, columnUUID string) (*KTypes.Column, int, error) {
+	var b *KTypes.Board
+	switch board := board.(type) {
+	case *KTypes.Board:
+		b = board
+	case string:
+		var err error
+		b, err = kanban.GetBoard(board)
+		if err != nil {
+			return nil, -1, fmt.Errorf("failed getting board: %v", err)
+		}
+
+	}
+	for i, column := range b.Columns {
+		if column.UUID == columnUUID {
+			return &column, i, nil
+		}
+	}
+
+	return nil, -1, fmt.Errorf("column not found")
+}
+
+func ExtractColumn(c *fiber.Ctx) (*KTypes.Board, *KTypes.Column, int, error) {
+	board, err := kanban.ExtractBoard(c)
+	if err != nil {
+		return nil, nil, -1, fmt.Errorf("failed extracting board: %v", err)
+	}
+
+	columnUUID := c.Params("column")
+
+	column, index, err := getColumn(board, columnUUID)
+
+	return board, column, index, nil
+}
