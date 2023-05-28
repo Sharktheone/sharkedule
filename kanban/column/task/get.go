8c079692292fc759abe4c0393be7a7cb5bb30ae3
Sharktheone <sharktheone@proton.me>
2023-05-28 23:13:12 +0200

    add missing extract task

--- kanban/column/task/get.go
+++ kanban/column/task/get.go
@@ -1,9 +1,20 @@
 package task
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"sharkedule/kanban"
 	"sharkedule/kanban/column"
 )
+
+func ExtractTask(c *fiber.Ctx) (*Task, error) {
+	_, co, err := column.ExtractColumn(c)
+	if err != nil {
+		return nil, err
+	}
+	taskUUID := c.Params("task")
+	return co.GetTask(taskUUID)
+
+}
 
 func (t *Task) GetParentBoard() (*kanban.Board, error) {
 	return kanban.GetBoard(t.Board)
