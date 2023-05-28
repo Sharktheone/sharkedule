9ef8225fd9b226c531dd9aad3746cd7c017f0460
Sharktheone <sharktheone@proton.me>
2023-05-28 22:01:44 +0200

    NewTask handler in Column

--- kanban/column/task.go
+++ kanban/column/task.go
@@ -13,3 +13,11 @@ func (c *Column) GetTask(uuid string) (*task.Task, int, error) {
 	}
 	return nil, -1, errors.New("task not found")
 }
+
+func (c *Column) NewTask(name string) *task.Task {
+	t := task.New(name)
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	c.Tasks = append(c.Tasks, t)
+	return t
+}
