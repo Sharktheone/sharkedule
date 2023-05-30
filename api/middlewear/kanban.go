42d8fe4b683fc962581b4ce0a812a3e23a6470a0
Sharktheone <sharktheone@proton.me>
2023-05-30 23:59:17 +0200

    add some middlewear
    should already be committed in 01f55a52

---
+++ api/middlewear/kanban.go
@@ -0,0 +1,37 @@
+package middlewear
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"sharkedule/kanban/board"
+	"sharkedule/kanban/board/column"
+	"sharkedule/kanban/board/column/task"
+)
+
+func ExtractTask(c *fiber.Ctx) (*task.Task, error) {
+	_, co, err := ExtractColumn(c)
+	if err != nil {
+		return nil, err
+	}
+	taskUUID := c.Params("task")
+	return co.GetTask(taskUUID)
+}
+
+func ExtractColumn(c *fiber.Ctx) (*board.Board, *column.Column, error) {
+	b, err := ExtractBoard(c)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed extracting board: %v", err)
+	}
+
+	column, err := b.GetColumn(c.Params("column"))
+	if err != nil {
+		return b, nil, fmt.Errorf("failed getting column: %v", err)
+	}
+
+	return b, column, nil
+}
+
+func ExtractBoard(c *fiber.Ctx) (*board.Board, error) {
+	boardUUID := c.Params("kanbanboard")
+	return board.GetBoard(boardUUID)
+}
