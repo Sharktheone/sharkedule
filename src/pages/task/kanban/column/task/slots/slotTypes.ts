457d8363468596d69feff2759283edcdb1d87a83
Sharktheone <sharktheone@proton.me>
2023-06-02 04:47:19 +0200

    add slot Types

---
+++ src/pages/task/kanban/column/task/slots/slotTypes.ts
@@ -0,0 +1,62 @@
+import {
+    kanbanDateDueType,
+    kanbanImageType,
+    kanbanMemberType,
+    kanbanPriorityType,
+    kanbanStatusType,
+    kanbanTagType
+} from "../../../types"
+
+
+export type Slot = TagsSlot | PrioritySlot | StatusSlot | DateDueSlot | StageSlot | MembersSlot | ProgressSlot | ImageSlot | SubtasksSlot | CustomFieldSlot
+
+
+export type TagsSlot = {
+    type: "tags"
+    tag: kanbanTagType[]
+}
+
+type PrioritySlot = {
+    type: "priority"
+    priority: kanbanPriorityType
+}
+
+type StatusSlot = {
+    type: "status"
+    status: kanbanStatusType
+}
+
+type DateDueSlot = {
+    type: "date_due"
+    due_date: kanbanDateDueType
+}
+
+type StageSlot = {
+    type: "stage"
+    stage: string //TODO: create kanbanStageType
+}
+
+type MembersSlot = {
+    type: "members"
+    members: kanbanMemberType[]
+}
+
+type ProgressSlot = {
+    type: "progress"
+    progress: number //TODO: create kanbanProgressType
+}
+
+type ImageSlot = {
+    type: "image"
+    image: kanbanImageType
+}
+
+type SubtasksSlot = {
+    type: "subtasks"
+    subtasks: number //TODO: create kanbanSubtasksType
+}
+
+type CustomFieldSlot = {
+    type: "custom_field"
+    custom_field: string //TODO: create kanbanCustomFieldType
+}
