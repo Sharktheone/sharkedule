4b3514644f9669213cd435b50543fe673c58bb63
Sharktheone <sharktheone@proton.me>
2023-06-02 23:48:10 +0200

    getSlotColor for task border and color

--- src/pages/task/kanban/column/task/slots/slotProvider.tsx
+++ src/pages/task/kanban/column/task/slots/slotProvider.tsx
@@ -1,5 +1,13 @@
 import {createContext, ReactNode} from "react"
-import {Configuration, IndexedSlot, Slot, SlotNames, TagsSlot} from "@kanban/column/task/slots/slotTypes"
+import {
+    Configuration, CustomFieldsSlot, DateDueSlot,
+    IndexedSlot,
+    PrioritySlot, ProgressSlot,
+    Slot,
+    SlotColors,
+    SlotNames, StageSlot, StatusSlot,
+    TagsSlot
+} from "@kanban/column/task/slots/slotTypes"
 import {kanbanTaskType} from "@kanban/types"
 
 type SlotContextType = {
@@ -53,7 +61,7 @@ function SlotProvider({children, task}: Props) {
         if (task.images) slots.images.images = task.images
         if (task.subtasks) slots.subtasks.subtasks = task.subtasks
         if (task.customFields) slots.custom_fields.custom_fields = task.customFields
-        if (task.checkList) slots.checklist.checklist = task.checkList
+        if (task.checkList) slots.checklists.checklist = task.checkList
 
         for (let slot in config.lower) {
             const slotName = config.lower[slot]
@@ -67,6 +75,61 @@ function SlotProvider({children, task}: Props) {
             if (s) upperSlot.push(s)
         }
         return
+    }
+
+    function getSlotColor(slot: Slot): string | null {
+        switch (slot.type) {
+            case SlotColors.PRIORITY: {
+                const prioritySlot = slot as PrioritySlot
+                if (prioritySlot.priority.color) return prioritySlot.priority.color
+                return null
+            }
+            case SlotColors.STATUS: {
+                const statusSlot = slot as StatusSlot
+                if (statusSlot.status.color) return statusSlot.status.color
+                return null
+            }
+            case SlotColors.DATE_DUE: {
+                const dateDueSlot = slot as DateDueSlot
+                const dateDue = dateDueSlot.due_date.date
+                const date = new Date(dateDue)
+                const today = new Date()
+                if (date > today) return "#00ff00" // TODO: Make configurable
+
+                if (date < today) return "#ff0000"
+                return null
+            }
+
+            case SlotColors.STAGE: {
+                const stageSlot = slot as StageSlot
+                if (stageSlot.stage.color) return stageSlot.stage.color
+                return null
+            }
+
+            case SlotColors.PROGRESS: {
+                const progressSlot = slot as ProgressSlot
+                const progress = progressSlot.progress.percentage
+                if (progress >= 100) return "#00ff00" // TODO: Make configurable
+                if (progress > 80) return "#d5ff18"
+                if (progress > 70) return "#ffff00"
+                if (progress > 60) return "#ffcc00"
+                if (progress > 30) return "#ff9900"
+                if (progress > 20) return "#ff6600"
+                if (progress > 10) return "#ff3300"
+                return "#ff0000"
+            }
+
+            case SlotColors.CUSTOM_FIELDS: {
+                return "#4433ff" // TODO: Make configurable which field to use
+            }
+
+            case SlotColors.CHECKLIST: {
+                return "#4433ff" // TODO: Make configurable which checklist to use
+            }
+
+            default:
+                return null
+        }
     }
 
     return (
