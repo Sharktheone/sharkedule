7f82d56e0d89c1a06528dd72eff761a82ce88bb7
Sharktheone <sharktheone@proton.me>
2023-06-04 02:05:44 +0200

    remove unneeded styling

--- src/pages/task/kanban/column/task/styles.ts
+++ src/pages/task/kanban/column/task/styles.ts
@@ -3,7 +3,6 @@ import {createStyles} from "@mantine/core"
 
 export const useStyles = createStyles((theme) => ({
     task: {
-        minHeight: "3rem",
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
         "&:hover": {
             backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
@@ -12,7 +11,6 @@ export const useStyles = createStyles((theme) => ({
             theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
         }`,
         borderRadius: theme.radius.md,
-        padding: theme.spacing.xs,
         boxShadow: theme.shadows.sm,
         transition: "height 200ms ease",
     }
