8bb7459b254ef7af2c8b5396fa8a871b9b3955ab
Sharktheone <sharktheone@proton.me>
2023-06-04 02:19:52 +0200

    implement all that shit for lower slot

--- src/pages/task/kanban/column/task/slots/lower/lowerSlot.tsx
+++ src/pages/task/kanban/column/task/slots/lower/lowerSlot.tsx
@@ -1,9 +1,23 @@
-
+import {useContext} from "react"
+import {SlotContext} from "@kanban/column/task/slots/slotProvider"
+import styles from "@kanban/column/task/slots/styles.module.scss"
+import {Slot} from "@kanban/column/task/slots/slotTypes"
+import Render from "@kanban/column/task/slots/render/render"
 
 
 export default function LowerSlot() {
+    const context = useContext(SlotContext)
+    let lowerSlot = context?.lowerSlot
+    if (!lowerSlot) return null
+
+    if (lowerSlot.length === 0) return null
+
 
     return (
-        <div> </div>
+        <div className={styles.slot}>
+            {lowerSlot.map((slot: Slot) => (
+                <Render key={slot.type} slot={slot}/>
+            ))}
+        </div>
     )
 }
