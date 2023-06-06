d18e366074e1133c0c0992bbfb8bf442dc6e1c16
Sharktheone <sharktheone@proton.me>
2023-06-06 03:32:58 +0200

    append if missing

--- kanban/v2/environment/environment.go
+++ kanban/v2/environment/environment.go
@@ -14,15 +14,24 @@ func GetTaskEnv(task *types2.Task) {
 
 }
 
+func AppendIfMissing(slice []string, s string) []string {
+	for _, ele := range slice {
+		if ele == s {
+			return slice
+		}
+	}
+	return append(slice, s)
+}
+
 // THOUGHTS about this:
 
-// Version 1
+// Version 1 xxxxxx
 // board -> getEnv (all fields[columns, tags, members, etc.]) -> push to buffer
 // -> getEnv for all columns in buffer -> push to buffer
 // -> getEnv for all tasks in buffer -> push to buffer
 // -> filter out duplicates -> update buffer
 
-// Version 2
+// Version 2 <<<<<
 // board -> getEnv (all fields[columns, tags, members, etc.]) -> push uuids to buffer
 // -> getEnv for all columns in buffer -> push non-duplicate uuids to buffer
 // -> getEnv for all tasks in buffer -> push non-duplicate uuids to buffer
