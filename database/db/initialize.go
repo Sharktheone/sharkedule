aec06d069cd34c5f876a58b218df92268868ad43
Sharktheone <sharktheone@proton.me>
2023-06-06 04:51:14 +0200

    initialize V2 DB

--- database/db/initialize.go
+++ database/db/initialize.go
@@ -3,16 +3,22 @@ package db
 import (
 	"github.com/Sharktheone/sharkedule/database"
 	"github.com/Sharktheone/sharkedule/database/jsonfile"
+	jsonfileV2 "github.com/Sharktheone/sharkedule/database/jsonfile/V2"
 	"log"
 )
 
 var (
-	DB database.IDatabase
+	DB   database.IDatabase
+	DBV2 database.IDatabaseV2
 )
 
 func Init() {
 	DB = jsonfile.NewJSONFile()
 	if err := DB.Load(); err != nil {
 		log.Fatalf("failed loading database: %v", err)
+	}
+	DBV2 = jsonfileV2.NewJSONFile()
+	if err := DBV2.Load(); err != nil {
+		log.Fatalf("failed loading database v2: %v", err)
 	}
 }
