c7643907457e76c38c4882bd61b51c11e2871431
Sharktheone <sharktheone@proton.me>
2023-06-06 23:44:18 +0200

    new kanbandb handlers

--- kanban/v2/database/get.go
+++ kanban/v2/database/get.go
@@ -47,3 +47,70 @@ func GetTask(tasks []*types2.Task, uuid string) (*types2.Task, error) {
 	}
 	return nil, fmt.Errorf("task with uuid %s does not exist", uuid)
 }
+
+func GetTags(tags []*types2.Tag) []*types2.Tag {
+	return tags
+}
+
+func GetTag(tags []*types2.Tag, uuid string) (*types2.Tag, error) {
+	for _, t := range tags {
+		if t.UUID == uuid {
+			return t, nil
+		}
+	}
+	return nil, fmt.Errorf("tag with uuid %s does not exist", uuid)
+}
+
+func GetStatus(status []*types2.Status, uuid string) (*types2.Status, error) {
+	for _, s := range status {
+		if s.UUID == uuid {
+			return s, nil
+		}
+	}
+	return nil, fmt.Errorf("status with uuid %s does not exist", uuid)
+}
+
+func GetPriority(priorities []*types2.Priority, uuid string) (*types2.Priority, error) {
+	for _, p := range priorities {
+		if p.UUID == uuid {
+			return p, nil
+		}
+	}
+	return nil, fmt.Errorf("priority with uuid %s does not exist", uuid)
+}
+
+func GetMember(members []*types2.Member, uuid string) (*types2.Member, error) {
+	for _, m := range members {
+		if m.UUID == uuid {
+			return m, nil
+		}
+	}
+	return nil, fmt.Errorf("member with uuid %s does not exist", uuid)
+}
+
+func GetChecklist(checklists []*types2.Checklist, uuid string) (*types2.Checklist, error) {
+	for _, c := range checklists {
+		if c.UUID == uuid {
+			return c, nil
+		}
+	}
+	return nil, fmt.Errorf("checklist with uuid %s does not exist", uuid)
+}
+
+func GetAttachment(attachments []*types2.Attachment, uuid string) (*types2.Attachment, error) {
+	for _, a := range attachments {
+		if a.UUID == uuid {
+			return a, nil
+		}
+	}
+	return nil, fmt.Errorf("attachment with uuid %s does not exist", uuid)
+}
+
+func GetDate(dates []*types2.Date, uuid string) (*types2.Date, error) {
+	for _, date := range dates {
+		if date.UUID == uuid {
+			return date, nil
+		}
+	}
+	return nil, fmt.Errorf("date with uuid %s does not exist", uuid)
+}
