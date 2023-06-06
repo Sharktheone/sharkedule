3209e1c90846766cd3d492d24aaf175fcc95dee0
Sharktheone <sharktheone@proton.me>
2023-06-06 04:34:54 +0200

    use indexer in GetXEnv

--- kanban/v2/environment/environment.go
+++ kanban/v2/environment/environment.go
@@ -3,15 +3,32 @@ package environment
 import types2 "github.com/Sharktheone/sharkedule/kanban/v2/types"
 
 func GetBoardEnv(board *types2.Board) {
+	env := Environment{
+		Environment: types2.Environment{
+			Boards: []*types2.Board{board},
+		},
+	}
+	env.Index()
 
 }
 
 func GetColumnEnv(column *types2.Column) {
+	env := Environment{
+		Environment: types2.Environment{
+			Columns: []*types2.Column{column},
+		},
+	}
+	env.Index()
 
 }
 
 func GetTaskEnv(task *types2.Task) {
-
+	env := Environment{
+		Environment: types2.Environment{
+			Tasks: []*types2.Task{task},
+		},
+	}
+	env.Index()
 }
 
 func AppendIfMissing(slice []*string, s *string) []*string {
