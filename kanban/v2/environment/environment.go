932bc5dd104300b4601a986dbc1323e2efa48aa7
Sharktheone <sharktheone@proton.me>
2023-06-07 03:44:54 +0200

    return env

--- kanban/v2/environment/environment.go
+++ kanban/v2/environment/environment.go
@@ -2,33 +2,37 @@ package environment
 
 import types2 "github.com/Sharktheone/sharkedule/kanban/v2/types"
 
-func GetBoardEnv(board *types2.Board) {
+func GetBoardEnv(board *types2.Board) *types2.Environment {
 	env := Environment{
-		Environment: types2.Environment{
+		Environment: &types2.Environment{
 			Boards: []*types2.Board{board},
 		},
 	}
 	env.Index()
 
+	return env.Environment
 }
 
-func GetColumnEnv(column *types2.Column) {
+func GetColumnEnv(column *types2.Column) *types2.Environment {
 	env := Environment{
-		Environment: types2.Environment{
+		Environment: &types2.Environment{
 			Columns: []*types2.Column{column},
 		},
 	}
 	env.Index()
 
+	return env.Environment
 }
 
-func GetTaskEnv(task *types2.Task) {
+func GetTaskEnv(task *types2.Task) *types2.Environment {
 	env := Environment{
-		Environment: types2.Environment{
+		Environment: &types2.Environment{
 			Tasks: []*types2.Task{task},
 		},
 	}
 	env.Index()
+
+	return env.Environment
 }
 
 // THOUGHTS about this:
