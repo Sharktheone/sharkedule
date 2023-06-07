bef81fa549360ce04f02c4c2080d7fdeaf249ae2
Sharktheone <sharktheone@proton.me>
2023-06-07 23:59:56 +0200

    remove unneeded handlers

--- kanban/v2/column/get.go
+++ kanban/v2/column/get.go
@@ -5,10 +5,6 @@ import (
 	"github.com/Sharktheone/sharkedule/database/types"
 )
 
-func (c *Column) GetParentBoard() (*types.Board, error) {
-	return db.DB.GetBoard(c.Board)
-}
-
 func GetColumn(board, column string) (*types.Column, error) {
 	return db.DB.GetColumn(board, column)
 }
