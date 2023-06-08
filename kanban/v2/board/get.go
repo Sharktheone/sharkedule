b75b1f4ddbf133971147b6951ffcb1fd437d0342
Sharktheone <sharktheone@proton.me>
2023-06-08 21:34:57 +0200

    move get board handlers

--- kanban/v2/board/get.go
+++ kanban/v2/board/get.go
@@ -1,33 +1,28 @@
-package getboard
+package board
 
 import (
 	"github.com/Sharktheone/sharkedule/database/db"
 	"github.com/Sharktheone/sharkedule/kanban/KTypes/namelist"
-	"github.com/Sharktheone/sharkedule/kanban/v2/board"
 )
 
-func Get(uuid string) (*board.Board, error) {
+func Get(uuid string) (*Board, error) {
 	b, err := db.DBV2.GetBoard(uuid)
 	if err != nil {
 		return nil, err
 	}
 
-	return &board.Board{Board: b}, nil
-	//if err != nil {
-	//	return nil, err
-	//}
-	//return environment.GetBoardEnv(board), nil
+	return &Board{Board: b}, nil
 }
 
-func GetBoards() ([]*board.Board, error) {
+func GetBoards() ([]*Board, error) {
 	boards, er := db.DBV2.GetBoards()
 	if er != nil {
 		return nil, er
 	}
 
-	var bds []*board.Board
+	var bds []*Board
 	for _, b := range boards {
-		bds = append(bds, &board.Board{Board: b})
+		bds = append(bds, &Board{Board: b})
 	}
 	return bds, nil
 }
