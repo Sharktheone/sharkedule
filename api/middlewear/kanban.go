b7845d202e9664c207ace4f03f0b2dc67aad5c2f
Sharktheone <sharktheone@proton.me>
2023-06-08 23:24:09 +0200

    update middleware

--- api/middlewear/kanban.go
+++ api/middlewear/kanban.go
@@ -1,37 +1,23 @@
 package middlewear
 
 import (
-	"fmt"
-	"github.com/Sharktheone/sharkedule/kanban/board"
-	"github.com/Sharktheone/sharkedule/kanban/board/column"
-	"github.com/Sharktheone/sharkedule/kanban/board/column/task"
+	"github.com/Sharktheone/sharkedule/kanban/v2/board"
+	"github.com/Sharktheone/sharkedule/kanban/v2/column"
+	"github.com/Sharktheone/sharkedule/kanban/v2/task"
 	"github.com/gofiber/fiber/v2"
 )
 
 func ExtractTask(c *fiber.Ctx) (*task.Task, error) {
-	_, co, err := ExtractColumn(c)
-	if err != nil {
-		return nil, err
-	}
-	taskUUID := c.Params("task")
-	return co.GetTask(taskUUID)
+	uuid := c.Params("task")
+	return task.Get(uuid)
 }
 
-func ExtractColumn(c *fiber.Ctx) (*board.Board, *column.Column, error) {
-	b, err := ExtractBoard(c)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed extracting board: %v", err)
-	}
-
-	col, err := b.GetColumn(c.Params("column"))
-	if err != nil {
-		return b, nil, fmt.Errorf("failed getting column: %v", err)
-	}
-
-	return b, col, nil
+func ExtractColumn(c *fiber.Ctx) (*column.Column, error) {
+	uuid := c.Params("column")
+	return column.Get(uuid)
 }
 
 func ExtractBoard(c *fiber.Ctx) (*board.Board, error) {
 	boardUUID := c.Params("kanbanboard")
-	return board.GetBoard(boardUUID)
+	return board.Get(boardUUID)
 }
