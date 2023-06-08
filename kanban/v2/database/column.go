d36fe226ac49fe5c46247ac6bc316e11ce778997
Sharktheone <sharktheone@proton.me>
2023-06-08 20:28:46 +0200

    move move-handler

--- kanban/v2/database/column.go
+++ kanban/v2/database/column.go
@@ -3,6 +3,7 @@ package kanbandb
 import (
 	"fmt"
 	"github.com/Sharktheone/sharkedule/kanban/v2/column"
+	"github.com/Sharktheone/sharkedule/kanban/v2/types"
 )
 
 func DeleteColumn(columns []*column.Column, uuid string) error {
@@ -46,4 +47,28 @@ func SaveColumn(columns []*column.Column, column *column.Column) error {
 
 func SaveColumns(columns []*column.Column, columnsToSave []*column.Column) {
 	columns = columnsToSave
+}
+
+func MoveColumn(board *types.Board, column string, toIndex int) error {
+	var (
+		deleted  = false
+		inserted = false
+	)
+	for index, c := range board.Columns {
+		if c == column {
+			board.Columns = append(board.Columns[:index], board.Columns[index+1:]...)
+			if inserted {
+				return nil
+			}
+			deleted = true
+		}
+		if index == toIndex {
+			board.Columns = append(board.Columns, c)
+			if deleted {
+				return nil
+			}
+			inserted = true
+		}
+	}
+	return nil
 }
