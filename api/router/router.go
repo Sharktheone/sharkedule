d1eeab0e7e102a16002a745ab3dc277a4dc961e0
Sharktheone <sharktheone@proton.me>
2023-06-09 04:15:08 +0200

    add some routes

--- api/router/router.go
+++ api/router/router.go
@@ -21,43 +21,68 @@ func Start() {
 
 	api := r.Group("api") // /api
 	{
-		boards := api.Group("kanban/board") // /api/kanban/board
+		kanban := api.Group("kanban") // /api/kanban
 		{
-			boards.Get("list", kanbanboard.List)            // GET /api/kanban/board/list
-			boards.Get("list/names", kanbanboard.ListNames) // GET /api/kanban/board/list/names
-			boards.Put("new", kanbanboard.Create)           // PUT /api/kanban/board/new
-
-			board := boards.Group(":kanbanboard") // /api/kanban/board/:kanbanboard
+			boards := kanban.Group("board") // /api/kanban/board
 			{
+				boards.Get("list", kanbanboard.List)            // GET /api/kanban/board/list
+				boards.Get("list/names", kanbanboard.ListNames) // GET /api/kanban/board/list/names
+				boards.Put("new", kanbanboard.Create)           // PUT /api/kanban/board/new
 
-				board.Get("", kanbanboard.Get)             // GET /api/kanban/board/:kanbanboard
-				board.Delete("delete", kanbanboard.Delete) // DELETE /api/kanban/board/:kanbanboard/delete
-
-				columns := board.Group("column") // /api/kanban/board/:kanban/board/column
+				board := boards.Group(":kanbanboard") // /api/kanban/board/:kanbanboard
 				{
-					columns.Put("new", column.Create) // PUT /api/kanban/board/:kanbanboard/column/new
 
-					col := columns.Group(":column") // /api/kanban/board/:kanbanboard/column/:column
+					board.Get("", kanbanboard.Get)             // GET /api/kanban/board/:kanbanboard
+					board.Delete("delete", kanbanboard.Delete) // DELETE /api/kanban/board/:kanbanboard/delete
+
+					columns := board.Group("column") // /api/kanban/board/:kanban/board/column
 					{
-						col.Get("", column.Get)             // GET /api/kanban/board/:kanbanboard/column/:column
-						col.Delete("delete", column.Delete) // DELETE /api/kanban/board/:kanbanboard/column/:column/delete
-						col.Patch("move", column.Move)      // PATCH /api/kanban/board/:kanbanboard/column/:column/move
+						columns.Put("new", column.Create) // PUT /api/kanban/board/:kanbanboard/column/new
 
-						tsk := col.Group("task") // /api/kanban/board/:kanbanboard/column/:column/task
+						col := columns.Group(":column") // /api/kanban/board/:kanbanboard/column/:column
 						{
-							tsk.Put("new", task.Create) // PUT /api/kanban/board/:kanbanboard/column/:column/task/new
+							col.Get("", column.Get)             // GET /api/kanban/board/:kanbanboard/column/:column
+							col.Delete("delete", column.Delete) // DELETE /api/kanban/board/:kanbanboard/column/:column/delete
+							col.Patch("move", column.Move)      // PATCH /api/kanban/board/:kanbanboard/column/:column/move
 
-							t := tsk.Group(":task") // /api/kanban/board/:kanbanboard/column/:column/task/:task
+							tsk := col.Group("task") // /api/kanban/board/:kanbanboard/column/:column/task
 							{
-								t.Patch("move", task.Move)      // PATCH /api/kanban/board/:kanbanboard/column/:column/task/:task/move
-								t.Get("", task.Get)             // GET /api/kanban/board/:kanbanboard/column/:column/task/:task
-								t.Delete("delete", task.Delete) // DELETE /api/kanban/board/:kanbanboard/column/:column/task/:task/delete
+								tsk.Put("new", task.Create) // PUT /api/kanban/board/:kanbanboard/column/:column/task/new
+
+								t := tsk.Group(":task") // /api/kanban/board/:kanbanboard/column/:column/task/:task
+								{
+									t.Patch("move", task.Move)      // PATCH /api/kanban/board/:kanbanboard/column/:column/task/:task/move
+									t.Get("", task.Get)             // GET /api/kanban/board/:kanbanboard/column/:column/task/:task
+									t.Delete("delete", task.Delete) // DELETE /api/kanban/board/:kanbanboard/column/:column/task/:task/delete
+								}
 							}
 						}
 					}
 				}
 			}
+			columns := kanban.Group("column") // /api/kanban/column
+			{
+				columns.Delete("delete", column.Delete) // DELETE /api/kanban/column/delete
+				columns.Patch("rename", column.Rename)  // PATCH /api/kanban/column/rename
+			}
+			tasks := kanban.Group("task") // /api/kanban/task
+			{
+				tasks.Delete("delete", task.Delete)   // DELETE /api/kanban/task/delete
+				tasks.Patch("rename", task.Rename)    // PATCH /api/kanban/task/rename
+				tasks.Put("tags", task.AddTags)       // PUT /api/kanban/task/tags
+				tasks.Delete("tags", task.DeleteTags) // DELETE /api/kanban/task/tags
+			}
+			tags := kanban.Group("tag") // /api/kanban/tag
+			{
+				tags.Put("new", tag.CreateTag)       // PUT /api/kanban/tag/new
+				tags.Delete("delete", tag.DeleteTag) // DELETE /api/kanban/tag/delete
+				tags.Patch("rename", tag.RenameTag)  // PATCH /api/kanban/tag/rename
+				tags.Patch("update", tag.Update)     // PATCH /api/kanban/tag/color
+				tags.Get("list", tag.List)           // GET /api/kanban/tag/list
+
+			}
 		}
+
 	}
 
 	web.Serve(r)
