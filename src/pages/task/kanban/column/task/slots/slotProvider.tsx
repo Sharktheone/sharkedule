d86f6568a07e6e759815cd57f30c803cb82a8ae8
Sharktheone <sharktheone@proton.me>
2023-06-16 22:27:12 +0200

    comment out broken stuff for now

--- src/pages/task/kanban/column/task/slots/slotProvider.tsx
+++ src/pages/task/kanban/column/task/slots/slotProvider.tsx
@@ -95,72 +95,69 @@ export function SlotProvider({children, task}: Props) {
         if (config.border) {
             const b = config.border
             const s = slots[b]
-            border = getSlotColor(s)
+            // border = getSlotColor(s) TODO
         }
 
         if (config.color) {
             const c = config.color
             const s = slots[c]
-            color = getSlotColor(s)
+            // color = getSlotColor(s) TODO
         }
 
         return {upperSlot, lowerSlot, border, color}
     }
 
-    function getSlotColor(slot: Slot): string | null {
-        switch (slot.type) {
-            case SlotColors.PRIORITY: {
-                const prioritySlot = slot as PrioritySlot
-                if (prioritySlot.priority.color) return prioritySlot.priority.color
-                return null
-            }
-            case SlotColors.STATUS: {
-                const statusSlot = slot as StatusSlot
-                if (statusSlot.status.color) return statusSlot.status.color
-                return null
-            }
-            case SlotColors.DATE_DUE: {
-                const dateDueSlot = slot as DateDueSlot
-                const dateDue = dateDueSlot.due_date.date
-                const date = new Date(dateDue)
-                const today = new Date()
-                if (date > today) return "#00ff00" // TODO: Make configurable
-
-                if (date < today) return "#ff0000"
-                return null
-            }
-
-            case SlotColors.STAGE: {
-                const stageSlot = slot as StageSlot
-                if (stageSlot.stage.color) return stageSlot.stage.color
-                return null
-            }
-
-            case SlotColors.PROGRESS: {
-                const progressSlot = slot as ProgressSlot
-                const progress = progressSlot.progress.percentage
-                if (progress >= 100) return "#00ff00" // TODO: Make configurable
-                if (progress > 80) return "#d5ff18"
-                if (progress > 70) return "#ffff00"
-                if (progress > 60) return "#ffcc00"
-                if (progress > 30) return "#ff9900"
-                if (progress > 20) return "#ff6600"
-                if (progress > 10) return "#ff3300"
-                return "#ff0000"
-            }
-
-            case SlotColors.CUSTOM_FIELDS: {
-                return "#4433ff" // TODO: Make configurable which field to use
-            }
-
-            case SlotColors.CHECKLIST: {
-                return "#4433ff" // TODO: Make configurable which checklist to use
-            }
-
-            default:
-                return null
-        }
-    }
+    // function getSlotColor(slot: Slot): string | null {
+    //         case SlotColors.PRIORITY: {
+    //             const prioritySlot = slot as PrioritySlot
+    //             if (prioritySlot.priority.color) return prioritySlot.priority.color
+    //             return null
+    //         }
+    //         case SlotColors.STATUS: {
+    //             const statusSlot = slot as StatusSlot
+    //             if (statusSlot.status.color) return statusSlot.status.color
+    //             return null
+    //         }
+    //         case SlotColors.DATE_DUE: {
+    //             const dateDueSlot = slot as DateDueSlot
+    //             const dateDue = dateDueSlot.due_date.date
+    //             const date = new Date(dateDue)
+    //             const today = new Date()
+    //             if (date > today) return "#00ff00" // TODO: Make configurable
+    //
+    //             if (date < today) return "#ff0000"
+    //             return null
+    //         }
+    //
+    //         case SlotColors.STAGE: {
+    //             const stageSlot = slot as StageSlot
+    //             if (stageSlot.stage.color) return stageSlot.stage.color
+    //             return null
+    //         }
+    //
+    //         if (slot instanceof ProgressSlot) {
+    //             const progressSlot = slot as ProgressSlot
+    //             const progress = progressSlot.progress.percentage
+    //             if (progress >= 100) return "#00ff00" // TODO: Make configurable
+    //             if (progress > 80) return "#d5ff18"
+    //             if (progress > 70) return "#ffff00"
+    //             if (progress > 60) return "#ffcc00"
+    //             if (progress > 30) return "#ff9900"
+    //             if (progress > 20) return "#ff6600"
+    //             if (progress > 10) return "#ff3300"
+    //             return "#ff0000"
+    //         }
+    //
+    //         case (slot. instanceof CustomFieldsSlot) {
+    //             return "#4433ff" // TODO: Make configurable which field to use
+    //         }
+    //
+    //         if (slot instanceof ChecklistsSlot) {
+    //             return "#4433ff" // TODO: Make configurable which checklist to use
+    //         }
+    //
+    //             return null
+    // }
 
     return (
         <SlotContext.Provider value={slotify()}>
