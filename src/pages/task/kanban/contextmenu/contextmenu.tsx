da14525621163bdffb88e0de3ee2583dcf8ea0cd
Sharktheone <sharktheone@proton.me>
2023-06-21 23:20:20 +0200

    animation shenanigans

--- src/pages/task/kanban/contextmenu/contextmenu.tsx
+++ src/pages/task/kanban/contextmenu/contextmenu.tsx
@@ -1,6 +1,6 @@
 
 import styles from "./styles.module.scss"
-import {CSSProperties} from "react"
+import {CSSProperties, useEffect, useRef, useState} from "react"
 import {useColors} from "./styles"
 import {Entries} from "@kanban/contextmenu/entries"
 
@@ -8,6 +8,7 @@ type Props = {
     x: number
     y: number
     open: boolean
+    close: () => void
     type: types
 }
 
@@ -16,8 +17,15 @@ export enum types {
     COLUMN
 }
 
-export default function ContextMenu({x, y, open}: Props)  {
+export default function ContextMenu({x, y, open, close}: Props)  {
     const {classes, cx} = useColors()
+    const [freshlyOpened, setFreshlyOpened] = useState(false)
+    const [freshlyClosed, setFreshlyClosed] = useState(false)
+    const [hidden, setHidden] = useState(true)
+    const contextMenuRef = useRef<HTMLDivElement>(null)
+
+
+
     function position():CSSProperties {
         return {
             top: `${y}px`,
@@ -25,11 +33,32 @@ export default function ContextMenu({x, y, open}: Props)  {
         }
     }
 
-    if (!open) return null
+    useEffect(() => {
+        if (open) {
+            setHidden(false)
+            setFreshlyOpened(true)
+            contextMenuRef?.current?.focus()
+            setTimeout(() => {
+                setFreshlyOpened(false)
+            }, 500)
+        } else {
+            setFreshlyClosed(true)
+            setTimeout(() => {
+                setFreshlyClosed(false)
+                setHidden(true)
+            }, 500)
+        }
+        console.log("open", open, "hidden", hidden, "freshlyOpened", freshlyOpened, "freshlyClosed", freshlyClosed)
+    }, [open])
+
+
+    if (hidden) return null
 
 
     return  (
-        <div className={`${styles.contextmenu} ${cx(classes.contextMenu)}`} style={position()}>
+        <div ref={contextMenuRef}
+            className={`${styles.contextmenu} ${cx(classes.contextMenu)} ${freshlyOpened ? styles.opened : ""} ${freshlyClosed ? styles.closed : ""}`} style={position()}>
+            <button onClick={close}/>
             {
                 Entries.map((entry, i) => (
                     <div className={styles.entry} key={i}>
