e1cd94702baa90fed2355690aab2b76b7e743d85
Sharktheone <sharktheone@proton.me>
2023-06-24 18:11:09 +0200

    change context menu color

--- src/pages/task/kanban/contextmenu/styles.ts
+++ src/pages/task/kanban/contextmenu/styles.ts
@@ -3,9 +3,10 @@ import {createStyles} from "@mantine/core"
 
 export const useColors = createStyles((theme) => ({
         contextMenu: {
-            backgroundColor: theme.colors.dark[3],
-            borderColor: theme.colors.dark[5]
-
+            backgroundColor: theme.colors.dark[4],
+            border: `1px solid ${
+                theme.colorScheme === "dark" ? theme.colors.dark[3] : theme.colors.gray[2]
+            }`,
         }
     }
 ))
