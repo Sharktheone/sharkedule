dcc8b1be8ed0e5b50e925bc097a9d3acdeeff381
Sharktheone <sharktheone@proton.me>
2023-06-25 20:46:37 +0200

    details thing for user preferences

---
+++ src/components/details/details.tsx
@@ -0,0 +1,32 @@
+import {ReactNode} from "react"
+import {details} from "@/components/details/types"
+import {Drawer, Modal} from "@mantine/core"
+
+
+type Props = {
+    children: ReactNode
+    open: boolean
+    onClose: () => void
+}
+
+
+
+//TODO: make changable
+let type = details.DRAWER
+
+export default function Details({children, onClose, open}: Props) {
+    if (type === details.DRAWER) {
+        return (
+            <Drawer opened={open} onClose={onClose}>
+                {children}
+            </Drawer>
+        )
+    } else {
+        return (
+            <Modal opened={open} onClose={onClose}>
+                {children}
+            </Modal>
+        )
+    }
+
+}
---
+++ src/components/details/types.ts
@@ -0,0 +1,5 @@
+export enum details {
+    DRAWER,
+    MODAL,
+    VIEW,
+}
