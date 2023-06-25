cd13e7b40a0bed8df060c6f0ce70412baaee0f1f
Sharktheone <sharktheone@proton.me>
2023-06-25 23:20:48 +0200

    change overlay props

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -16,13 +16,17 @@ let type = details.DRAWER
 export default function Details({children, onClose, open}: Props) {
     if (type === details.DRAWER) {
         return (
-            <Drawer opened={open} onClose={onClose} position="right">
+            <Drawer opened={open} onClose={onClose} position="right"
+                    overlayProps={{opacity: 0.15, blur: 1}}
+            >
                 {children}
             </Drawer>
         )
     } else {
         return (
-            <Modal opened={open} onClose={onClose}>
+            <Modal opened={open} onClose={onClose}
+                   overlayProps={{opacity: 0.15, blur: 1}}
+            >
                 {children}
             </Modal>
         )
