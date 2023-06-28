0a702a2aef6447f352beee4b901fad03f5e0464c
Sharktheone <sharktheone@proton.me>
2023-06-28 17:36:08 +0200

    move getTag into RenderTag

--- src/pages/task/kanban/column/task/slots/render/tags/renderTag.tsx
+++ src/pages/task/kanban/column/task/slots/render/tags/renderTag.tsx
@@ -1,36 +1,29 @@
-import {TagsSlot} from "@kanban/column/task/slots/slotTypes"
-import {kanbanTagType} from "@kanban/types"
 import styles from "@kanban/column/task/slots/render/tags/styles.module.scss"
 import {getTag} from "@/pages/task/utils/tag"
 import {Tag} from "@kanban/types2"
 
 
 type Props = {
-    tagUUIDs: string[]
+    tags: string[]
 }
 
-export default function RenderTags({tagUUIDs}: Props) {
-
-    let tags = tagUUIDs.map((uuid) => {
-        return getTag(uuid) as Tag
-    })
-
-
-
+export default function RenderTags({tags}: Props) {
     return (
         <div>
-            {tags?.map((tag: Tag) => (
-                <RenderTag key={tag.uuid} tag={tag}/>
+            {tags?.map((tag) => (
+                <RenderTag key={tag} uuid={tag}/>
             ))}
         </div>
     )
 }
 
 type RenderTagProps = {
-    tag: kanbanTagType
+    uuid: string
 }
 
-export function RenderTag({tag}: RenderTagProps) {
+export function RenderTag({uuid}: RenderTagProps) {
+    let tag: Tag = getTag(uuid) ?? {name: "Error, Tag not found", color: "#000000"} as Tag
+
     return (
         <div className={styles.tag} style={{backgroundColor: `${tag.color}80`, borderColor: tag.color}}>
             {tag.name}
