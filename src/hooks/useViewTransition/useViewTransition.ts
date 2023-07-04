a82563c0a0eedc545e241aaecd2297524183649f
Sharktheone <sharktheone@proton.me>
2023-07-04 23:51:33 +0200

    fix props

--- src/hooks/useViewTransition/useViewTransition.ts
+++ src/hooks/useViewTransition/useViewTransition.ts
@@ -2,22 +2,13 @@ import {useEffect, useState} from "react"
 import styles from "./styles.module.scss"
 
 
-type Props = {
-    currentView: string
-
-    viewList: [{
-        id: string
-        element: HTMLElement
-    }]
-
-    duration?: number // TODO
-    transition?: "slideX" | "slideY" | "rotate-left" | "rotate-right" | "flipX" | "flipY" // TODO
-    timingFunction?: string // TODO
-
+export type viewRef = {
+    id: string
+    element: HTMLElement
 }
 
 
-export default function useViewTransition({currentView, viewList}: Props) {
+export default function useViewTransition(currentView: string, viewList: viewRef[], duration?: number, transition?: string, timingFunction?: string) {
     const [firstRender, setFirstRender] = useState(true)
     const [lastView, setLastView] = useState<string | null>(null)
     useEffect(() => {
@@ -38,13 +29,12 @@ export default function useViewTransition({currentView, viewList}: Props) {
 
     function hide(element: HTMLElement) {
         element.classList.remove(styles.active)
-        element.classList.add(styles.hidden)
-        //TODO: transition
+        element.classList.add(styles.hidden, styles[transition ?? ""])
     }
 
     function show(element: HTMLElement) {
         element.classList.remove(styles.hidden)
-        element.classList.add(styles.active)
+        element.classList.add(styles.active, styles[transition ?? ""])
         //TODO: transition
     }
 
