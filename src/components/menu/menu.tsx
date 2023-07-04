d7edc2f673462a47a7dc1ad4a19add51585b798a
Sharktheone <sharktheone@proton.me>
2023-07-04 12:32:14 +0200

    Component Structure for Menu Components

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,6 +1,14 @@
-import {ReactNode} from "react"
+import {Dispatch, ReactNode, SetStateAction} from "react"
 import styles from "./menu.module.css"
 
+
+type Props = {
+    children: ReactNode
+    width?: number
+    open?: boolean
+    setOpen?: Dispatch<SetStateAction<boolean>>
+
+}
 export function Menu() {
     return (
         <div>
@@ -27,6 +35,39 @@ export namespace Menu {
     }
 
 
+    type ComponentStructureProps = {
+        children: ReactNode
+        icon?: ReactNode
+        label?: ReactNode
+        color?: string
+        className?: string
+    }
+    function ComponentStructure({children, icon, label, color, className}: ComponentStructureProps) {
+        function getColor() {
+            return {
+                backgroundColor: color
+            }
+        }
+
+        return (
+            <div className={`${styles.component} ${className}`}>
+                {icon ?
+                    <div className={styles.icon}>
+                        {icon}
+                    </div>
+                    : null
+                }
+                {children}
+                {label ?
+                    <div className={styles.label}>
+                        {label}
+                    </div>
+                    : null
+                }
+            </div>
+        )
+    }
+
     type ItemProps = {
         children: ReactNode
         icon?: ReactNode
@@ -37,31 +78,13 @@ export namespace Menu {
     }
 
     export function Item({children, icon, label, color}: ItemProps) {
-        function getColor() {
-            return {
-                backgroundColor: color
-            }
-        }
-
-
         return (
-            <div className={styles.labelComponent}>
-                {icon ?
-                    <div className={styles.icon}>
-                        {icon}
-                    </div>
-                    : null
-                }
+            <ComponentStructure className={styles.item}
+                                icon={icon} label={label} color={color}>
                 <div className={styles.item}>
                     {children}
                 </div>
-                {label ?
-                    <div className={styles.label}>
-                        {label}
-                    </div>
-                    : null
-                }
-            </div>
+            </ComponentStructure>
         )
     }
 
@@ -80,23 +103,12 @@ export namespace Menu {
 
     export function Section({children, icon, label}: SectionProps) {
         return (
-            <div className={styles.labelComponent}>
-                {icon ?
-                    <div className={styles.icon}>
-                        {icon}
-                    </div>
-                    : null
-                }
+            <ComponentStructure className={styles.labelComponent}
+                                icon={icon} label={label}>
                 <div className={styles.labelName}>
                     {children}
                 </div>
-                {label ?
-                    <div className={styles.label}>
-                        {label}
-                    </div>
-                    : null
-                }
-            </div>
+            </ComponentStructure>
         )
     }
 }
