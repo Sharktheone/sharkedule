6fab0f1fa703926f0016c8348db37dd32b400610
Sharktheone <sharktheone@proton.me>
2023-07-08 23:58:06 +0200

    add focus outline

--- src/pages/task/kanban/column/task/details/colors.ts
+++ src/pages/task/kanban/column/task/details/colors.ts
@@ -3,10 +3,11 @@ import {createStyles} from "@mantine/core"
 
 export const useColors = createStyles((theme) => ({
     description: {
-        color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.colors.gray[7],
+        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[7],
         border: `1px solid ${
             theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
         }`,
-    }
+        "--outline-color": theme.colors.blue[5],
+    },
 
 }))
