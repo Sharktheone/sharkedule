87be03a063ffa28e18235514e606ac5ebc6348db
Sharktheone <sharktheone@proton.me>
2023-07-12 21:06:19 +0200

    change loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,10 +1,16 @@
 import styles from "./styles.module.scss"
 import {useColors} from "./colors"
 
-export default function Loader() {
+type Props = {
+    size?: string
+}
+
+export default function Loader({size}: Props) {
     const {cx, classes} = useColors()
     return (
-        <div className={`${styles.loader} ${cx(classes.loader)}`}>
+        <div className={`${styles.loader} ${cx(classes.loader)}`} style={{
+            height: size, width: size, borderWidth: `calc(${size} / 10)`
+        }}>
             Loading...
         </div>
     )
