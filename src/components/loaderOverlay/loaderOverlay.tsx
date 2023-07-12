be56c28227592d3920966433395f261ed4c3d5b0
Sharktheone <sharktheone@proton.me>
2023-07-12 21:26:51 +0200

    loaderOverlay

---
+++ src/components/loaderOverlay/loaderOverlay.tsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.scss'
+import Loader from "@/components/loader/loader"
+import {ReactNode} from "react"
+
+type Props = {
+    loading: boolean
+    children: ReactNode
+}
+
+export default function LoaderOverlay({loading, children}: Props) {
+    return (
+        <div className={styles.overlay}>
+            {children}
+            {loading &&
+                <div className={styles.loader}>
+                    <Loader size="30px"/>
+                </div>
+            }
+        </div>
+    )
+
+
+
+}
