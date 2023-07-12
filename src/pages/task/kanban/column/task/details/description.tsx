a5d206cfcc9fb98320d9a37d3429d730b719aa35
Sharktheone <sharktheone@proton.me>
2023-07-12 21:27:06 +0200

    description loaderOverlay

--- src/pages/task/kanban/column/task/details/description.tsx
+++ src/pages/task/kanban/column/task/details/description.tsx
@@ -2,7 +2,7 @@ import {useContext} from "react"
 import {EnvironmentContext} from "@kanban/environment"
 import {useColors} from "./colors"
 import styles from "./styles.module.scss"
-import Loader from "@/components/loader/loader"
+import LoaderOverlay from "@/components/loaderOverlay/loaderOverlay"
 
 type Props = {
     uuid: string
@@ -18,13 +18,13 @@ export default function Description({uuid}: Props) {
     if (!task) return null
 
     return (
-        <>
-
-            <Loader/>
-            <textarea className={`${cx(classes.description)} ${styles.description}`}>
-            {task.description}
-        </textarea>
-        </>
+        <div className={styles.wrapper}>
+            <LoaderOverlay loading={true}>
+                <textarea className={`${cx(classes.description)} ${styles.description}`}>
+                    {task.description}
+                </textarea>
+            </LoaderOverlay>
+        </div>
     )
 
 }
