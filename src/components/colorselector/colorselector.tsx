cf210d5412f3b606e682c26a42d4f54f0b498342
Sharktheone <sharktheone@proton.me>
2023-08-12 22:53:26 -0400

    update TODOs

---
+++ src/components/colorselector/colorselector.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+function ColorSelector() {
+    // TODO: This is ride now just a note to me...
+    // Maybe add some configurable colors that are predefined and also allow user defined colors.
+    // For the custom also add places to store them and also allow "single-use" colors, but maybe list them somewhere.
+    // Also for the custom colors first when you define them only let them change the hsl h-value, and add a extend button for the whole spectrum
+    return (
+        <></>
+    )
+}
