324267e869f7b4e5e807adf1af5c629c33dd3e3b
Sharktheone <sharktheone@proton.me>
2023-09-13 09:11:32 +0200

    add border

--- src/components/colorselector/colors.ts
+++ src/components/colorselector/colors.ts
@@ -4,5 +4,8 @@ import {createStyles} from "@mantine/core"
 export const useColors = createStyles((theme) => ({
     selector: {
         backgroundColor: theme.colors.dark[6],
+        border: `1px solid ${
+            theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
+        }`,
     }
 }))
