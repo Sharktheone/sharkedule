1cb552c547fcfd9f76463cac25db1d29a3cda7c8
Sharktheone <sharktheone@proton.me>
2023-09-16 23:56:34 +0200

    add some more "todos"

--- src/components/colorselector/colorselector.tsx
+++ src/components/colorselector/colorselector.tsx
@@ -13,6 +13,8 @@ export function ColorSelector() {
     // Maybe add some configurable colors that are predefined and also allow user defined colors.
     // For the custom also add places to store them and also allow "single-use" colors, but maybe list them somewhere.
     // Also for the custom colors first when you define them only let them change the hsl h-value, and add a extend button for the whole spectrum
+    // Selector for switching between normal and custom colors
+    // Add button to hide 1/2 of the color shades
 
     // (use viewTransition for this one, but maybe let the option, so we can use this as a "popup" variant - user-defined?
 
