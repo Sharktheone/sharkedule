f63634bbd38d95d26c81242189b5ff163923c530
Sharktheone <sharktheone@proton.me>
2023-09-22 11:31:47 +0200

    usePreviousState hook

---
+++ src/hooks/usePreviousState/usePreviousState.ts
@@ -0,0 +1,14 @@
+import {useState} from "react"
+
+
+export default function usePreviousState<S = undefined>(initial: S) {
+    const [state, setState] = useState<S>(initial)
+    const [previousState, setPreviousState] = useState<S | undefined>(undefined)
+
+    function updateState(newState: S) {
+        setPreviousState(state)
+        setState(newState)
+    }
+
+    return [state, updateState, previousState] as const
+}
