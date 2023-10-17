bf114bf98bd25b8d00fa9b701fae03bd63067f66
Sharktheone <sharktheone@proton.me>
2023-10-17 09:39:20 +0200

    add settings structs

--- user/user.go
+++ user/user.go
@@ -1,14 +1,20 @@
 package user
 
+import (
+	"github.com/Sharktheone/sharkedule/user/email"
+	"github.com/Sharktheone/sharkedule/user/mfa"
+	"github.com/Sharktheone/sharkedule/user/oauth"
+	"github.com/Sharktheone/sharkedule/user/settings"
+)
+
 type User struct {
-	UUID     string
-	Username string
-	Password string
-	//OAuth    OAuth?
-	//MFA	  	MFA?
-	//Email        Email
+	UUID         string
+	Username     string
+	Password     string
+	OAuth        oauth.OAuth
+	MFA          mfa.MFA
+	Email        email.EMail
 	Boards       []string
 	CustomColors []string
-	//Settings     Settings
-
+	Settings     settings.Settings
 }
