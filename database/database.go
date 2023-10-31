645b96c051b012d7682a2136d38f69f79cc21267
Sharktheone <sharktheone@proton.me>
2023-10-31 14:39:50 +0100

    group database functions

--- database/database.go
+++ database/database.go
@@ -16,43 +16,54 @@ type DBStructure struct {
 }
 
 type IDatabase interface {
+	// Load and Save
 	Load() error
 	Save() error
+
+	//Board functions
+	CreateBoard(name string) (*types.Board, error)
 	SaveBoard(board *types.Board) error
 	SaveBoards(boards []*types.Board) error
-	SaveColumn(column *types.Column) error
-	SaveColumns(columns []*types.Column) error
-	SaveTask(task *types.Task) error
-	SaveTasks(tasks []*types.Task) error
-	CreateBoard(name string) (*types.Board, error)
 	GetBoard(uuid string) (*types.Board, error)
 	GetAllBoards() ([]*types.Board, error)
 	GetBoards(uuids []string) ([]*types.Board, error)
 	GetAllBoardNames() ([]*namelist.NameList, error)
 	GetBoardNames(uuids []string) (names []*namelist.NameList, err error)
+	DeleteBoard(uuid string) error
+
+	//Column functions
+	SaveColumn(column *types.Column) error
+	SaveColumns(columns []*types.Column) error
 	GetColumn(uuid string) (*types.Column, error)
+	DeleteColumnOnBoard(board, column string) error
+	RenameColumn(column, name string) error
+	DeleteColumn(uuid string) error
+	MoveColumn(board, uuid string, toIndex int) error
+	NewColumn(board, name string) (*types.Column, error)
+
+	//Task functions
+	SaveTask(task *types.Task) error
+	SaveTasks(tasks []*types.Task) error
 	GetTask(uuid string) (*types.Task, error)
+	DeleteTaskOnColumn(column, uuid string) error
+	DeleteTask(uuid string) error
+	MoveTask(column, uuid, toColumn string, toIndex int) error
+	NewTask(column, name string) (*types.Task, error)
+	RenameTask(task, name string) error
+	RemoveTagOnTask(column, uuid string) error
+	SetTagsOnTask(task string, tags []string) error
+	SetTaskDescription(task, description string) error
+
+	//Tag functions
 	GetAllTags() ([]*types.Tag, error)
 	GetTag(uuid string) (*types.Tag, error)
+	AddTagToTask(task, tag string) error
+
+	//Other functions
 	GetStatus(uuid string) (*types.Status, error)
 	GetPriority(uuid string) (*types.Priority, error)
 	GetMember(uuid string) (*types.Member, error)
 	GetChecklist(uuid string) (*types.Checklist, error)
 	GetAttachment(uuid string) (*types.Attachment, error)
 	GetDate(uuid string) (*types.Date, error)
-	DeleteBoard(uuid string) error
-	DeleteColumn(uuid string) error
-	MoveColumn(board, uuid string, toIndex int) error
-	DeleteTask(uuid string) error
-	MoveTask(column, uuid, toColumn string, toIndex int) error
-	DeleteTaskOnColumn(column, uuid string) error
-	NewTask(column, name string) (*types.Task, error)
-	NewColumn(board, name string) (*types.Column, error)
-	AddTagToTask(task, tag string) error
-	RemoveTagOnTask(column, uuid string) error
-	SetTagsOnTask(task string, tags []string) error
-	SetTaskDescription(task, description string) error
-	RenameTask(task, name string) error
-	DeleteColumnOnBoard(board, column string) error
-	RenameColumn(column, name string) error
 }
