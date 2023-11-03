3e42026989705242b1d8040d37d422d8a80be93a
Sharktheone <sharktheone@proton.me>
2023-11-03 21:26:08 +0100

    ExtractUser middleware placeholder

---
+++ api/middleware/user.go
@@ -0,0 +1,11 @@
+package middleware
+
+import (
+	"github.com/Sharktheone/sharkedule/user"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ExtractUser(c *fiber.Ctx) (*user.User, error) {
+	//TODO
+	return nil, nil
+}
