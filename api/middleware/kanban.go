aec93e762e7e6656eede58d304763031e0681744
Sharktheone <sharktheone@proton.me>
2023-11-04 19:02:37 +0100

    do user access / perms directly in the extract handler

--- api/middleware/kanban.go
+++ api/middleware/kanban.go
@@ -4,23 +4,47 @@ import (
 	"github.com/Sharktheone/sharkedule/kanban/board"
 	"github.com/Sharktheone/sharkedule/kanban/column"
 	"github.com/Sharktheone/sharkedule/kanban/task"
+	"github.com/Sharktheone/sharkedule/user"
 	"github.com/gofiber/fiber/v2"
 )
 
-func ExtractTask(c *fiber.Ctx) (*task.Task, error) {
+func ExtractTask(c *fiber.Ctx) (*user.User, *task.Task, error) {
 	workspace := c.Params("workspace")
 	uuid := c.Params("task")
-	return task.Get(workspace, uuid)
+
+	u, err := ExtractUser(c)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	t, err := u.Access.GetTask(workspace, uuid)
+	return u, t, err
 }
 
-func ExtractColumn(c *fiber.Ctx) (*column.Column, error) {
+func ExtractColumn(c *fiber.Ctx) (*user.User, *column.Column, error) {
 	workspace := c.Params("workspace")
 	uuid := c.Params("column")
-	return column.Get(workspace, uuid)
+
+	u, err := ExtractUser(c)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	col, err := u.Access.GetColumn(workspace, uuid)
+
+	return u, col, err
 }
 
-func ExtractBoard(c *fiber.Ctx) (*board.Board, error) {
+func ExtractBoard(c *fiber.Ctx) (*user.User, *board.Board, error) {
 	workspace := c.Params("workspace")
-	boardUUID := c.Params("board")
-	return board.Get(workspace, boardUUID)
+	uuid := c.Params("board")
+
+	u, err := ExtractUser(c)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	brd, err := u.Access.GetBoard(workspace, uuid)
+
+	return u, brd, err
 }
