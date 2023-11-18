9f2d655bd9ae3e13199fd127517b224248c7a688
Sharktheone <sharktheone@proton.me>
2023-11-18 22:21:29 +0100

    fix Link in home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 export default function Home() {
     return (
         <div>
-            <Link to="/kanbanboard">Tasks</Link>
+            <Link to="/board">Tasks</Link>
         </div>
     )
 }
