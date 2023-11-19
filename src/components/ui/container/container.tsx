2a5f44a41df61e143292667969296e049130706d
Sharktheone <sharktheone@proton.me>
2023-11-19 23:04:09 +0100

    container component

--- src/components/ui/container/container.tsx
+++ src/components/ui/container/container.tsx
@@ -1,8 +1,15 @@
 
+type props = {
+    noBorder?: boolean
+    noBackground?: boolean
+    noPadding?: boolean
+} & React.HTMLAttributes<HTMLDivElement>
 
 
-export default function Container() {
+export default function Container({noBorder, noBackground, noPadding, ...props}: props) {
     return (
-        <div/>
+        <div
+            className={`container ${noBorder ? "no-border" : ""} ${noBackground ? "no-background" : ""} ${noPadding ? "no-padding" : ""}`}
+            {...props}/>
     )
 }
