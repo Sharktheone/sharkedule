816c4d086d6f30e9e22d587a255d9f3ff04e9408
Sharktheone <sharktheone@proton.me>
2023-11-30 22:41:40 +0100

    initial input

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,7 +1,60 @@
+import {HTMLAttributes} from "react"
+
+type Props = {
+    radius?: true | "sm" | "md" | "lg" | "xl"
+    tooltip?: string
+    error?: string
+    required?: boolean
+} & HTMLAttributes<HTMLInputElement>
+
+export function Input({radius, tooltip, error, required, className, ...props}: Props) {
+
+    let classes = className ?? ""
+    switch (radius) {
+        case "sm":
+            classes += " rounded-sm"
+            break
+        case "md":
+            classes += " rounded-md"
+            break
+        case "lg":
+            classes += " rounded-lg"
+            break
+        case "xl":
+            classes += " rounded-xl"
+            break
+        case true:
+            classes += " rounded"
+            break
+        default:
+            classes += ""
+            break
+    }
+
+    if (tooltip) {
+        classes += " tooltip"
+    }
+
+    if (error) {
+        classes += " error"
+    }
+
+    if (required) {
+        classes += " required"
+    }
 
 
-export function Input() {
     return (
-        <div/>
+        <>
+        <input {...props}/>
+            {
+                tooltip &&
+                <span className="tooltip-text">{tooltip} - TODO</span>
+            }
+            {
+                error &&
+                <span className="error-text">{error} - TODO</span>
+            }
+        </>
     )
 }
