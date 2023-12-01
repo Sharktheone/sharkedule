4d54cfe7c03a4c80fabe6e27f6ecd69ebc148ac5
Sharktheone <sharktheone@proton.me>
2023-12-01 21:30:38 +0100

    tooltip todo

---
+++ src/components/tooltip/tooltip.tsx
@@ -0,0 +1,15 @@
+
+type Props = {
+    show?: boolean
+    text?: string
+    position?: "top" | "bottom" | "left" | "right"
+}
+
+export default function Tooltip() {
+    return (
+        <div>
+            <h1>Tooltip</h1>
+            <p>TODO: Tooltip</p>
+        </div>
+    )
+}
