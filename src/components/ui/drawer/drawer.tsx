4e701cfb4489f357c2308ffb4051b5168bb73d3b
Sharktheone <sharktheone@proton.me>
2023-12-09 23:04:36 +0100

    some markup code for segmented control and drawer

--- src/components/ui/drawer/drawer.tsx
+++ src/components/ui/drawer/drawer.tsx
@@ -1,4 +1,22 @@
-export function Drawer() {
+import {HTMLAttributes} from "react"
+
+type Props = {
+    opened: boolean
+    onClose: () => void
+    position: "top" | "bottom" | "left" | "right"
+    about?: string
+    title?: string
+    overlayProps?: {
+        opacity: number
+        blur: number
+    }
+
+} & HTMLAttributes<HTMLDivElement>
+
+
+export function Drawer({...props}: Props) {
+
+    //TODO: render in portal
     return (
         <div/>
     )
--- src/components/ui/segmentedcontrol/segmentedcontrol.tsx
+++ src/components/ui/segmentedcontrol/segmentedcontrol.tsx
@@ -1,4 +1,12 @@
-export function SegmentedControl() {
+import {Dispatch, HTMLAttributes, SetStateAction} from "react"
+
+type Props = {
+    data: { label: string, value: string }[]
+    onChange: (value: string) => void | Dispatch<SetStateAction<string>>
+    value: string
+} & HTMLAttributes<HTMLDivElement>
+
+export function SegmentedControl({data}: Props) {
     return (
         <div/>
     )
