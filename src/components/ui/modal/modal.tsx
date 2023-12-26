3f844c00f5d33e97d4866326755cebca36b848c6
Sharktheone <sharktheone@proton.me>
2023-12-26 20:03:58 +0100

    first modal

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,5 +1,7 @@
 import {DetailedHTMLProps, HTMLAttributes} from "react"
 import {createPortal} from "react-dom"
+import styles from "./modal.module.scss"
+import {CloseButton, Title} from "@/components/ui"
 
 type Props = {
     show?: boolean
@@ -13,10 +15,20 @@ type Props = {
 } & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
 
-export function Modal({show, onClose, title, opened, ...props}: Props) {
+export function Modal({show, onClose, title, opened, children, ...props}: Props) {
 
-    //TODO: Render this in a portal
-    return createPortal(<>
+    // if (!show) return null
 
-    </>, document.body)
+    return createPortal(<div className={styles.modalBackdrop}>
+        <div className={styles.modal}>
+            <div className={styles.modalHeader}>
+                <Title s={3}>{title}</Title>
+                <CloseButton onClick={onClose}/>
+            </div>
+            <div className={styles.modalBody}>
+                {children}
+            </div>
+        </div>
+
+    </div>, document.body)
 }
