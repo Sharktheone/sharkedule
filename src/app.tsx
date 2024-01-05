b56477b265c0b489ba8de2efcc933600b0c9e68c
Sharktheone <sharktheone@proton.me>
2024-01-05 12:06:57 +0100

    fix react-toastify styles

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,13 @@ import "./styles/color-modifiers.scss"
 import {RouterProvider} from "react-router-dom"
 import {router} from "@/router/router"
 import {ToastContainer} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export default function App() {
 
     return (
         <>
-            <ToastContainer position="bottom-left" autoClose={5000} newestOnTop draggable theme="dark" pauseOnHover/>
+            <ToastContainer position="bottom-right" autoClose={5000} newestOnTop draggable theme="dark" pauseOnHover/>
             <RouterProvider router={router}/>
         </>
 
