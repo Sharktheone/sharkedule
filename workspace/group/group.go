3d57a5e0e5c5d4e3b5a0f02b743da0dd9c02f8dd
Sharktheone <sharktheone@proton.me>
2024-01-17 21:45:41 +0100

    add some more group things

--- workspace/group/group.go
+++ workspace/group/group.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Group struct {
-	UUID        string `json:"uuid" bson:"uuid" yaml:"uuid"`
-	Name        string `json:"name" bson:"name" yaml:"name"`
-	Description string `json:"description" bson:"description" yaml:"description"`
-	Icon        string `json:"icon" bson:"icon" yaml:"icon"`
-	IconColor   string `json:"icon_color" bson:"icon_color" yaml:"icon_color"`
+	UUID        string   `json:"uuid" bson:"uuid" yaml:"uuid"`
+	Name        string   `json:"name" bson:"name" yaml:"name"`
+	Description string   `json:"description" bson:"description" yaml:"description"`
+	Icon        string   `json:"icon" bson:"icon" yaml:"icon"`
+	IconColor   string   `json:"icon_color" bson:"icon_color" yaml:"icon_color"`
+	Items       []string `json:"items" bson:"items" yaml:"items"`
 
 	Fields []field.Field `json:"fields" bson:"fields" yaml:"fields"`
 }
@@ -29,4 +30,41 @@ func (g *Group) GetType() elements.Type {
 
 func (g *Group) GetFields() []field.Field {
 	return g.Fields
+}
+
+func (g *Group) GetDescription() string {
+	return g.Description
+}
+
+func (g *Group) GetIcon() string {
+	return g.Icon
+}
+
+func (g *Group) GetIconColor() string {
+	return g.IconColor
+}
+
+func (g *Group) GetItems() []string {
+	return g.Items
+}
+
+func (g *Group) AddItem(item string) {
+	g.Items = append(g.Items, item)
+}
+
+func (g *Group) RemoveItem(item string) {
+	for i, it := range g.Items {
+		if it == item {
+			g.Items = append(g.Items[:i], g.Items[i+1:]...)
+			return
+		}
+	}
+}
+
+func (g *Group) UpdateIcon(icon string) {
+	g.Icon = icon
+}
+
+func (g *Group) UpdateIconColor(color string) {
+	g.IconColor = color
 }
