b42b2454a75f6e9ddbdd3208f172d711d828f392
Sharktheone <sharktheone@proton.me>
2024-01-22 22:43:02 +0100

    initial element struct

--- element/element.go
+++ element/element.go
@@ -3,11 +3,24 @@ package element
 import (
 	"github.com/Sharktheone/sharkedule/element/elements"
 	"github.com/Sharktheone/sharkedule/field"
+	"github.com/Sharktheone/sharkedule/kanban/activity"
 )
 
-type Element interface {
-	GetUUID() string
-	GetName() string
-	GetType() elements.Type
-	GetFields() []field.Field
+type Element struct {
+	UUID     string            `json:"uuid" bson:"uuid" yaml:"uuid"`
+	Type     elements.Type     `json:"type" bson:"type" yaml:"type"`
+	Fields   []field.Field     `json:"fields" bson:"fields" yaml:"fields"`
+	Activity activity.Activity `json:"activity" bson:"activity" yaml:"activity"`
+}
+
+func (e *Element) GetUUID() string {
+	return e.UUID
+}
+
+func (e *Element) GetType() elements.Type {
+	return e.Type
+}
+
+func (e *Element) GetFields() []field.Field {
+	return e.Fields
 }
