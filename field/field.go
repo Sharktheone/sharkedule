26c71b0776e66ba137bc3a041a84738d62f470b6
Sharktheone <sharktheone@proton.me>
2024-01-24 20:19:08 +0100

    add more field types

--- field/field.go
+++ field/field.go
@@ -15,10 +15,44 @@ type Field struct {
 type Type string
 
 const (
-	String     Type = "string"
-	Number     Type = "number"
-	Date       Type = "date"
-	MultiList  Type = "multiList"
-	SingleList Type = "singleList"
-	Iteration  Type = "iteration"
+	Text          Type = "text"
+	MultilineText Type = "multilineText"
+	Number        Type = "number"
+	Date          Type = "date"
+	MultiList     Type = "multiList"
+	SingleList    Type = "singleList"
+	Iteration     Type = "iteration"
+	Checkbox      Type = "checkbox"
+	Link          Type = "link"
+	Email         Type = "email"
+	Phone         Type = "phone"
+	Location      Type = "location"
+	Progress      Type = "progress"
+	File          Type = "file"
+	Dependency    Type = "dependency"
+	Dependents    Type = "dependents"
+	Assignee      Type = "assignee"
+	Assignees     Type = "assignees"
+	Rating        Type = "rating"
+	Time          Type = "time"
+	Team          Type = "team"
+	Color         Type = "color"
+	Month         Type = "month"
+	Week          Type = "week"
+	Year          Type = "year"
+	Day           Type = "day"
+	Range         Type = "range"
+
+	Action Type = "action"
+
+	//Element types
+	Element Type = "element"
+	Project Type = "project"
+	Group   Type = "group"
+	Board   Type = "board"
+	Column  Type = "column"
+	Task    Type = "task"
+	Event   Type = "event"
+	//Workspace     Type = "workspace" - not sure about this one
+
 )
