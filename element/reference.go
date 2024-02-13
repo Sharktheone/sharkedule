f7717a82ee169975bccae3c80a076d7071b3c633
Shark <sharktheone@proton.me>
2024-02-13 17:28:12 +0100

    update interfaces

--- element/reference.go
+++ element/reference.go
@@ -1,5 +1,9 @@
 package element
 
+import (
+	"github.com/Sharktheone/sharkedule/database/db"
+)
+
 type Reference struct {
 	UUID   string   `json:"uuid" bson:"uuid" yaml:"uuid"`
 	Field  []string `json:"field" bson:"field" yaml:"field"`
@@ -8,16 +12,21 @@ type Reference struct {
 
 type ReferenceGroup struct {
 	refs []Reference
-	ws   string   //Workspace Handle
-	e    *Element //Element Handle
+	ws   string //Workspace Handle
+	e    string //Element Handle
 }
 
 func (rg *ReferenceGroup) AddReference(r Reference) {
-	//rg.ws.GetElement(r.UUID).ReferencedBy.AddReference(Reference{
-	//	UUID:   rg.e.UUID,
-	//	Field:  r.Field,
-	//	Linked: r.Linked,
-	//})
+	elem, err := db.DB.GetElement(rg.ws, r.UUID)
+	if err != nil {
+		return
+	}
+
+	elem.ReferencedBy.AddReference(Reference{
+		UUID:   rg.e,
+		Field:  r.Field,
+		Linked: r.Linked,
+	})
 
 	rg.refs = append(rg.refs, r)
 }
--- types/types.go
+++ types/types.go
@@ -15,19 +15,25 @@ type Element interface {
 	GetUUID() string
 	GetType() ElementType
 	GetFields() []Field
+	GetField(f string) Field
+	UpdateField(f string, v string) error
 	GetActivity() int //TODO
 	GetReferencedBy() ReferenceGroup
 	GetReferences() ReferenceGroup
+	AddReference(r Reference)
+	AddReferencedBy(r Reference)
 	GetUser() string
 	SetUser(u string)
 	GetWorkspace() string
-	SetWorkspace(ws string)
+	SetWorkspace(ws string) //only for db
 }
 
 type Workspace interface {
 	GetUUID() string
 	GetName() string
 	GetElement(u string) Element
+	CreateElement(t ElementType, name string) (*Element, error)
+	GetElements(u []string) []Element
 }
 
 type Reference interface {
