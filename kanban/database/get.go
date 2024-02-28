ba9701bf5bb55a40bc03140712aefd801ce4a0d4
Shark <sharktheone@proton.me>
2024-02-28 20:27:20 +0100

    fix method undefined on pointer to interface

--- kanban/database/get.go
+++ kanban/database/get.go
@@ -5,7 +5,7 @@ import (
 	"github.com/Sharktheone/sharkedule/types"
 )
 
-func GetWorkspace(workspaces []*types.Workspace, uuid string) (*types.Workspace, error) {
+func GetWorkspace(workspaces []types.Workspace, uuid string) (types.Workspace, error) {
 	for _, w := range workspaces {
 		if w.GetUUID() == uuid {
 			return w, nil
@@ -14,7 +14,7 @@ func GetWorkspace(workspaces []*types.Workspace, uuid string) (*types.Workspace,
 	return nil, fmt.Errorf("workspace with uuid %s does not exist", uuid)
 }
 
-func DeleteWorkspace(workspaces []*types.Workspace, uuid string) error {
+func DeleteWorkspace(workspaces []types.Workspace, uuid string) error {
 	for i, w := range workspaces {
 		if w.GetUUID() == uuid {
 			workspaces = append(workspaces[:i], workspaces[i+1:]...)
@@ -24,7 +24,7 @@ func DeleteWorkspace(workspaces []*types.Workspace, uuid string) error {
 	return fmt.Errorf("workspace with uuid %s does not exist", uuid)
 }
 
-func GetElement(elements []*types.Element, uuid string) (*types.Element, error) {
+func GetElement(elements []types.Element, uuid string) (types.Element, error) {
 	for _, e := range elements {
 		if e.GetUUID() == uuid {
 			return e, nil
@@ -33,14 +33,14 @@ func GetElement(elements []*types.Element, uuid string) (*types.Element, error) 
 	return nil, fmt.Errorf("element with uuid %s does not exist", uuid)
 }
 
-func CreateElement(elements []*types.Element, elementType *types.ElementType, name string) (*types.Element, error) {
-	var e *types.Element //TODO
+func CreateElement(elements []types.Element, elementType types.ElementType, name string) (types.Element, error) {
+	var e types.Element //TODO
 	elements = append(elements, e)
 	return e, nil
 }
 
-func GetElements(elements []*types.Element, uuids []string) ([]*types.Element, error) {
-	var es []*types.Element
+func GetElements(elements []types.Element, uuids []string) ([]types.Element, error) {
+	var es []types.Element
 	for _, uuid := range uuids {
 		e, err := GetElement(elements, uuid)
 		if err != nil {
